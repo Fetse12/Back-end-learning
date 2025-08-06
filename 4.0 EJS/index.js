0a63a5c011e45df59ad9ae609266e205fe5b940c
Fetse12 <fitsumteka2019@gmail.com>
2025-08-06 17:24:45 +0300

    new

--- 4.0 EJS/index.js
+++ 4.0 EJS/index.js
@@ -4,20 +4,26 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
-const today = new Date();
-const day = today.getDay();
 
-if (day === 0 || day === 6) {
-  console.log("It's a weekend!");
-} else {
-  console.log("It's a weekday.");
-}
 
 app.get('/', (req, res) => {
+
+    const today = new Date();
+    const day = today.getDay(); 
+
+    let type= "a weekday"
+    let advice="Keep pushing forward!"
+
+    if (day === 0 || day === 6) {
+        type;
+    } else {
+        type
+    }
+
   res.render('index.ejs', {
-    daytype: (day === 0 || day === 6) ? "weekend" : "weekday",
-    advice: (day === 0 || day === 6) ? "Enjoy your rest!" : "Keep pushing forward!"
-  });
+     daytype: "weekday", 
+     advice: "Keep pushing forward!" 
+     });
 });
 
 app.listen(port, () => {
