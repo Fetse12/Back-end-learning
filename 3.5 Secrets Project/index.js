e2d3e7ef6b5dba4913ab8f7a8310068927c49fd1
Fetse12 <fitsumteka2019@gmail.com>
2025-08-06 14:26:19 +0300

    Refactor authorization logic for improved clarity

--- 3.5 Secrets Project/index.js
+++ 3.5 Secrets Project/index.js
@@ -1,11 +1,9 @@
-//To see how the final website should work, run "node solution.js".
-//Make sure you have installed all the dependencies with "npm i".
-//The password is ILoveProgramming
 import express from "express";
 import bodyParser from "body-parser";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const app = express();
 const port = 3000;
 
@@ -13,12 +11,14 @@ var userIsAuthorised = false;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-function pascheck(req , res , next){
+function pascheck(req , res , next) {
     const password = req.body["password"];
     if (password === "ILoveProgramming") {
     userIsAuthorised = true;
   }
+  next();
 };
+
 app.use(pascheck);
 
 app.get("/", (req, res) => {
